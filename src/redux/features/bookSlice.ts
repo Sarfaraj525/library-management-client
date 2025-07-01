b5d45abb0d8cc8145ae9405f622f5c91133d20f7
Sarfaraj Nawaz Chowdhury <sarfarajeee525@gmail.com>
2025-07-02 01:00:26 +0600

    first commit

---
+++ src/redux/features/bookSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const initialState = {
+    value: 0,
+};
+
+
+export const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    
+  }
+})
+
+
+
+
+
+export default bookSlice.reducer
